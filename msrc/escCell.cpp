067a332cead03fb2f7a91c8480beb43a89b805da
Daniel <danielgorbea@hotmail.com>
2021-01-31 14:06:57 +0100

    Fix bug

--- msrc/escCell.cpp
+++ msrc/escCell.cpp
@@ -4,11 +4,11 @@ EscCell::EscCell() {}
 
 uint8_t EscCell::setCellCount(float voltage)
 {
-    float level[] = {4.2, 8.4, 12.6, 16.8, 21, 25.2, 29.4, 33.6, 33.6, 42, 42};
-    int8_t cont = 10;
-    while (voltage < level[cont])
+    float level[] = {0, 4.2, 8.4, 12.6, 16.8, 21, 25.2, 29.4, 33.6, 33.6, 42, 42};
+    int8_t cont = 11;
+    while (voltage < level[cont] && cont > 0)
         cont--;
-    return cont + 2;
+    return cont + 1;
 }
 
 float *EscCell::cellVoltageP()
