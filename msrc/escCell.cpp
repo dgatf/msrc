318ad8bb8aadbb74b50fa1e5dc3e337cee9dae20
Daniel Gorbea <danielgorbea@gmail.com>
2021-09-01 09:29:17 -0500

    Optimize cell count

--- msrc/escCell.cpp
+++ msrc/escCell.cpp
@@ -4,7 +4,7 @@ EscCell::EscCell() {}
 
 uint8_t EscCell::setCellCount(float voltage)
 {
-    float level[] = {0, 4.2, 8.4, 12.6, 16.8, 21, 25.2, 29.4, 33.6, 33.6, 42, 42};
+    float level[] = {0, 4.35, 8.7, 13.05, 17.4, 21.75, 26.1, 30.45, 34.8, 34.8, 43.5, 43.5};
     int8_t cont = 11;
     while (voltage < level[cont] && cont > 0)
         cont--;
