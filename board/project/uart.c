6b5fbebd26844ad3397eb5bbccbb3f3ff2928d71
NilsRo <n.rottgardt@gmail.com>
2023-01-24 18:18:47 +0100

    Handled UART Inverted with pulldown

--- board/project/uart.c
+++ board/project/uart.c
@@ -36,15 +36,18 @@ void uart1_begin(uint baudrate, uint gpio_tx, uint gpio_rx, uint timeout, uint d
         uart_alarm_pool = alarm_pool_create(2, 10);
     uart_init(uart1, baudrate);
     uart_set_fifo_enabled(uart1, false);
-    gpio_pull_up(gpio_rx);
     gpio_set_function(gpio_tx, GPIO_FUNC_UART);
     gpio_set_function(gpio_rx, GPIO_FUNC_UART);
 
     if (inverted)
     {
+        gpio_pull_down(gpio_rx);
         gpio_set_outover(gpio_tx, GPIO_OVERRIDE_INVERT);
         gpio_set_inover(gpio_rx, GPIO_OVERRIDE_INVERT);
     }
+    else
+        gpio_pull_up(gpio_rx);
+
     uart_set_format(uart1, databits, stopbits, parity);
     irq_set_exclusive_handler(UART1_IRQ, uart1_rx_handler);
     irq_set_enabled(UART1_IRQ, true);
