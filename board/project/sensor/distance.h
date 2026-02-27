e4ba46f576b68621ae108078cb2b0034eedacc61
Daniel <danielgorbea@gmail.com>
2026-02-27 09:55:27 -0500

    Fix distance flags

--- board/project/sensor/distance.h
+++ board/project/sensor/distance.h
@@ -4,8 +4,8 @@
 #include "common.h"
 
 typedef struct distance_parameters_t {
-    float *distance, *latitude, *longitude, *altitude, *sat, *fix, *hdop; // fix internal msrc: 0 no fix, 1 2D fix, 2 3D fix
-    bool *home_set;
+    float *distance, *latitude, *longitude, *altitude, *sat, *hdop;
+    uint8_t *fix, *home_set; // fix internal msrc: 0 no fix, 1 2D fix, 2 3D fix
 
 } distance_parameters_t;
 
